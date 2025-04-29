fba894703c47a3b1be8f53d83d8bab8b3abdae3b
Alwin42 <alwinemmanuel424@gmail.com>
2025-04-29 23:49:11 +0530

    title change

--- scripts.js
+++ scripts.js
@@ -22,7 +22,7 @@ async function checkWeather(city) {
 
     if(data.weather[0].main == "Clouds")
     {
-      weatherIcon.src="assets/Clouds.png";
+      weatherIcon.src="assets/clouds.png";
     }
     else if(data.weather[0].main == "Clear")
     {
